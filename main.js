3913d2177922b399227c7aa9cfff8fb80e66b40f
Matthew Herbst <mherbst@chegg.com>
2015-10-20 21:33:57 -0700

    buttons work kind of

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-var currSelection = '07-14';
+var currYear = "2015";
+var currMonth = "6";
 $(document).ready(function(){
   $('.selButton').click(function(event){
     console.log('Button clicked: ', event);
-    currSelection = event.currentTarget.value;
+    var currSelection = event.currentTarget.value;
+    var yearMonth = currSelection.split('-');
+    currMonth = yearMonth[0];
+    currYear = yearMonth[1];
+    console.log(currMonth);
+    console.log(currYear);
+    main();
   });
 });
+main();
 
 
 var renderer = new THREE.WebGLRenderer();
@@ -23,121 +31,123 @@ camera.lookAt(new THREE.Vector3(x, y, 0));
 var scene = new THREE.Scene();
 
 
-
-
-
-
-d3.json('county.json', function(data) {
-  d3.csv('data.csv', function(bigdata) {
-    var dict = {};
-
-    // bigdata = bigdata.filter(function(d) {
-    //   return d.Year == '2014';
-    // });
-
-    bigdata.forEach(function(d) {
-      dict[d.Zip] = d.Amount;
-    })
-
-    console.log('data', dict);
-
-    var counties = topojson.feature(data, data.objects.county);
-    var projection = d3.geo.mercator();
-    var features = counties.features;
-
-    var scale  = d3.scale.linear().domain([0, 130000]).range([0, 100]);
-
-    
-
-
-    light = new THREE.PointLight(0xffffff, x, y);
-    light.position.x = 170;
-    light.position.y = -70;
-    light.position.z = 50;
-    light.intensity = 1.5;
-
-
-    features.forEach(function(feature) {
-      var coordinates = feature.geometry.coordinates[0];
-      if (!coordinates) {
-        return;
-      }
-      var polygon = coordinates[0];
-      var geometry = new THREE.Geometry();
-
-      polygon.forEach(function(coords, i) {
-        var point = projection(coords);
-
-        geometry.vertices.push(new THREE.Vector3(point[0], -1*point[1], 0));
+function main() {
+  d3.json('county.json', function(data) {
+    d3.csv('data.csv', function(bigbigdata) {
+      console.log("big!",bigbigdata[0]);
+      console.log(bigbigdata.length);
+      var bigdata = bigbigdata.filter(function(ele, i) {
+        return ele.Year == currYear && ele.Month == currMonth;
       });
+      console.log(bigdata);
+      console.log(bigdata.length);
+      var dict = {};
+
+      // bigdata = bigdata.filter(function(d) {
+      //   return d.Year == '2014';
+      // });
+
+      bigdata.forEach(function(d) {
+        dict[d.Zip] = d.Amount;
+      })
+
+      console.log('data', dict);
+
+      var counties = topojson.feature(data, data.objects.county);
+      var projection = d3.geo.mercator();
+      var features = counties.features;
+
+      var scale  = d3.scale.linear().domain([0, 130000]).range([0, 100]);
 
 
-      var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({color: 0xffffff}));
-
-      scene.add(line);
-    });
-
-    features = features.filter(function(f) {
-      return dict[f.id];
-    });
-
-    scene.add(light);
-
-    features.forEach(function(feature) {
-      var coordinates = feature.geometry.coordinates[0];
-      if (!coordinates) {
-        return;
-      }
-      var polygon = coordinates[0];
 
 
-      var shape = new THREE.Shape();
+      light = new THREE.PointLight(0xffffff, x, y);
+      light.position.x = 170;
+      light.position.y = -70;
+      light.position.z = 50;
+      light.intensity = 1.5;
 
-      polygon.forEach(function(coords, i) {
-        var point = projection(coords);
 
-        if (i == 0) {
-          shape.moveTo(point[0], -1*point[1], 0);
-        } else {
-          shape.lineTo(point[0], -1*point[1], 0);
+      features.forEach(function(feature) {
+        var coordinates = feature.geometry.coordinates[0];
+        if (!coordinates) {
+          return;
         }
+        var polygon = coordinates[0];
+        var geometry = new THREE.Geometry();
+
+        polygon.forEach(function(coords, i) {
+          var point = projection(coords);
+
+          geometry.vertices.push(new THREE.Vector3(point[0], -1*point[1], 0));
+        });
+
+
+        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({color: 0xffffff}));
+
+        scene.add(line);
       });
 
-      // var material = new THREE.LineBasicMaterial({ color: 0xffffff });
+      features = features.filter(function(f) {
+        return dict[f.id];
+      });
 
-      material = new THREE.MeshLambertMaterial( { color: 0x78D8FF  } );
+      scene.add(light);
 
-      // var geometry = new THREE.ShapeGeometry(shape);
-      var amount;
-      if (dict[feature.id]) {
-        amount = scale(dict[feature.id]);
-      } else {
-        amount = 1;
-      }
-      var geometry = new THREE.ExtrudeGeometry(shape, { amount: amount });
+      features.forEach(function(feature) {
+        var coordinates = feature.geometry.coordinates[0];
+        if (!coordinates) {
+          return;
+        }
+        var polygon = coordinates[0];
 
-      mesh = new THREE.Mesh( geometry, material );
 
-      scene.add(mesh);
+        var shape = new THREE.Shape();
 
-      // var line = new THREE.Line(geometry, material);
-      // scene.add(line);
+        polygon.forEach(function(coords, i) {
+          var point = projection(coords);
 
-      // var shape = new THREE.ExtrudeGeometry(geometry, { amount: 20 });
-      // scene.add(shape);
-      renderer.render(scene, camera);
+          if (i == 0) {
+            shape.moveTo(point[0], -1*point[1], 0);
+          } else {
+            shape.lineTo(point[0], -1*point[1], 0);
+          }
+        });
 
-    })
+        // var material = new THREE.LineBasicMaterial({ color: 0xffffff });
+
+        material = new THREE.MeshLambertMaterial( { color: 0x78D8FF  } );
+
+        // var geometry = new THREE.ShapeGeometry(shape);
+        var amount;
+        if (dict[feature.id]) {
+          amount = scale(dict[feature.id]);
+        } else {
+          amount = 1;
+        }
+        var geometry = new THREE.ExtrudeGeometry(shape, { amount: amount });
+
+        mesh = new THREE.Mesh( geometry, material );
+
+        scene.add(mesh);
+
+        // var line = new THREE.Line(geometry, material);
+        // scene.add(line);
+
+        // var shape = new THREE.ExtrudeGeometry(geometry, { amount: 20 });
+        // scene.add(shape);
+        renderer.render(scene, camera);
+
+      })
+    });
+
+
+
+    // var generator = d3.geo.path().projection(d3.geo.mercator());
+
+    // var result = generator(counties);
+
+    // console.log('result', result);
   });
-
-
-
-  // var generator = d3.geo.path().projection(d3.geo.mercator());
-
-  // var result = generator(counties);
-
-  // console.log('result', result);
-
-
-
-});
+}
